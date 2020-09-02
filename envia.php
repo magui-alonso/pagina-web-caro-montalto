0177d79e230b8a4ed36cd0a28066f3bd3334ab25
magui-alonso <magui.alonso.g@gmail.com>
2020-09-02 14:02:20 -0300

    mail

--- envia.php
+++ envia.php
@@ -1,6 +1,6 @@
 ﻿<?php
 $remitente = $_POST['email'];
-$destinatario = 'magui.alonso.g@gmail.com'; // en esta línea va el mail del destinatario.
+$destinatario = 'info@caromontalto.com'; // en esta línea va el mail del destinatario.
 $asunto = 'Consulta'; // acá se puede modificar el asunto del mail
 if (!$_POST){
 ?>
